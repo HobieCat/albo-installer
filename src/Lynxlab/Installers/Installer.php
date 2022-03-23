6d53b437a4c6dd6c4ef61ac8800de766c4dfd826
Giorgio Consorti <g.consorti@lynxlab.com>
2022-03-23 17:38:56 +0100

    add install

--- src/Lynxlab/Installers/Installer.php
+++ src/Lynxlab/Installers/Installer.php
@@ -50,6 +50,18 @@ class Installer extends LibraryInstaller
      */
     public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
     {
+        $type = $package->getType();
+        $frameworkType = $this->findFrameworkType($type);
+
+        if ($frameworkType !== false) {
+            $class = 'Lynxlab\\Installers\\' . $this->supportedTypes[$frameworkType];
+            if (method_exists($class, 'install')) {
+                $this->io->write('****** DOING INSTALL ********');
+                $installer = new $class($package, $this->composer, $this->getIO());
+                $installer->install($repo, $package);
+            }
+        }
+
         return parent::install($repo, $package);
     }
 
--- src/Lynxlab/Installers/ItaliagovInstaller.php
+++ src/Lynxlab/Installers/ItaliagovInstaller.php
@@ -2,10 +2,18 @@
 
 namespace Lynxlab\Installers;
 
+use Composer\Package\PackageInterface;
+use Composer\Repository\InstalledRepositoryInterface;
+
 class ItaliagovInstaller extends BaseInstaller
 {
     /** @var array<string, string> */
     protected $locations = array(
         'extrafiles' => 'themes/custom/italiagov/{$name}/',
     );
+
+    public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
+    {
+        $this->io->write("ItaliagovInstaller install");
+    }
 }
