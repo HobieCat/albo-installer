fd006ae35ed2689997e15a9c77088fcc634c02e5
Giorgio Consorti <g.consorti@lynxlab.com>
2022-03-23 17:54:54 +0100

    fix

--- src/Lynxlab/Installers/ItaliagovInstaller.php
+++ src/Lynxlab/Installers/ItaliagovInstaller.php
@@ -16,6 +16,15 @@ class ItaliagovInstaller extends BaseInstaller
 
     public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
     {
-        $this->io->write(\sprintf("ItaliagovInstaller installed in %s", $this->getInstallPath($package, self::frameworkType)));
+        $basePath = $this->getInstallPath($package, self::frameworkType);
+        if (!\is_file($basePath.'../italiagov.libraries.yml.original')) {
+            if (\rename($basePath.'../italiagov.libraries.yml', $basePath.'../italiagov.libraries.yml.original')) {
+                $this->io->write("italiagov.libraries.yml successfully renamed to italiagov.libraries.yml.original!");
+            }
+        }
+        if (\copy($basePath.'italiagov.libraries.yml', $basePath.'../italiagov.libraries.yml')) {
+            $this->io->write("italiagov.libraries.yml successfully copied to parent dir!");
+        }
+        // $this->io->write(\sprintf("ItaliagovInstaller installed in %s", $this->getInstallPath($package, self::frameworkType)));
     }
 }
